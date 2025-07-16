a51ed0ab01f02cdc6b109ce8dfacff80e7586999
KisaliThumara <kisalithumara2002@gmail.com>
2025-07-16 21:22:24 +0530

    remove comments

--- components/Map.tsx
+++ components/Map.tsx
@@ -21,62 +21,7 @@ export default function Map(){
     const {directionCoordinates, routeTime} = useBicycle();
     console.log('Time: ', routeTime);
 
-   // const [bikePoints, setBikePoints] = useState<any[]>([]);
-
-    // useEffect(() => {
-    //     const gpsQuery = query(
-    //     ref(database, 'gps_data'),
-    //     orderByChild('timestamp'),
-    //     limitToLast(1)
-    // );
-
-    //     const unsubscribe = onValue(gpsQuery, (snapshot) => {
-    //         const data = snapshot.val();
-    //         console.log("🔥 Firebase response:", data);
-
-    //         if (data) {
-    //         const [key, latestRaw] = Object.entries(data)[0];
-    //         const latest = latestRaw as { latitude?: number; longitude?: number };
-
-    //         if (latest.latitude && latest.longitude) {
-    //             const latestPoint = point([latest.longitude, latest.latitude], { id: 'bike01' });
-    //             setBikePoints([latestPoint]);
-    //         } else {
-    //             console.warn("⚠️ Data exists but missing lat/lon:", latest);
-    //         }
-    //         } else {
-    //         console.warn("⚠️ No data received from Firebase.");
-    //         }
-    //     });
-
-    //     return () => unsubscribe();
-    //     }, []);
-
-
-    // useEffect(() => {
-    //     const locationRef = ref(database, 'gps_data');
-    //     const unsubscribe = onValue(locationRef, (snapshot) => {
-    //         const data = snapshot.val();
-    //         if (data) {
-    //           const points = Object.entries(data).map(([bikeId, coords]) => {
-    //             if (
-    //               typeof coords === 'object' && coords !== null && 
-    //               'latitude' in coords && 'longitude' in coords
-    //             ) {
-    //               const { latitude, longitude } = coords as { latitude: number; longitude: number };
-    //               if (latitude && longitude) {
-    //                 return point([longitude, latitude], { id: bikeId });
-    //               }
-    //             }
-    //           }).filter(Boolean); // remove undefined/null
-    //           setBikePoints(points);
-    //           }});
-
-        // Cleanup subscription on unmount
-    //     return () => unsubscribe();
-    // }, []);
-
-    
+      
     return (
         <MapView style={{flex:1}} styleURL="mapbox://styles/mapbox/navigation-night-v1">
             <Camera followZoomLevel={16} followUserLocation />
